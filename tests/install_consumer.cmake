baeb0dec306997ad9760a0e141f63ec665a7b715
Peter <Vanderhell@gmail.com>
2026-06-30 20:10:36 +0200

    Fix install consumer toolchain propagation

--- tests/install_consumer.cmake
+++ tests/install_consumer.cmake
@@ -5,8 +5,25 @@ endif()
 set(prefix "${BUILD_DIR}/_microassert_install")
 set(consumer_source "${PROJECT_SOURCE_DIR}/tests/fixtures/install_consumer")
 set(consumer_build "${BUILD_DIR}/_microassert_install_consumer")
+set(configure_args)
 
 file(REMOVE_RECURSE "${consumer_build}")
+
+if (DEFINED GENERATOR AND NOT GENERATOR STREQUAL "")
+    list(APPEND configure_args -G "${GENERATOR}")
+endif()
+if (DEFINED CMAKE_C_COMPILER AND NOT CMAKE_C_COMPILER STREQUAL "")
+    list(APPEND configure_args -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER})
+endif()
+if (DEFINED CMAKE_CXX_COMPILER AND NOT CMAKE_CXX_COMPILER STREQUAL "")
+    list(APPEND configure_args -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER})
+endif()
+if (DEFINED CMAKE_BUILD_TYPE AND NOT CMAKE_BUILD_TYPE STREQUAL "")
+    list(APPEND configure_args -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
+endif()
+if (DEFINED CMAKE_TOOLCHAIN_FILE AND NOT CMAKE_TOOLCHAIN_FILE STREQUAL "")
+    list(APPEND configure_args -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
+endif()
 
 execute_process(
     COMMAND "${CMAKE_COMMAND}" --install "${BUILD_DIR}" --prefix "${prefix}"
@@ -19,11 +36,10 @@ endif()
 
 execute_process(
     COMMAND "${CMAKE_COMMAND}"
-        -G Ninja
         -S "${consumer_source}"
         -B "${consumer_build}"
-        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
         -DCMAKE_PREFIX_PATH=${prefix}
+        ${configure_args}
     RESULT_VARIABLE configure_result
     OUTPUT_VARIABLE configure_output
     ERROR_VARIABLE configure_output)
